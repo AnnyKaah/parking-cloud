bb5acc0ac12ba8530b5eac48e2cc7ca19305f8a7
Anny Karoline <karoliny_anny@hotmail.com>
2022-08-10 10:22:43 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/parkingcloud/ParkingCloudApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.parkingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingCloudApplication.class, args);
+	}
+
+}
